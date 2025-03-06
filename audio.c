1c9dbf82d5d8d0b52a1617664e1de153138c5eeb
SiPhoung <152601939+SiPhoung@users.noreply.github.com>
2025-03-06 09:19:43 +0630

    Initial Commit

---
+++ audio.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "audio.h"
+
+// 音声ファイルを読み込む関数
+int readAudioFile(const char* filename, AudioData* audio) {
+    return 0;
+}
+
+// 音声再生関数
+void playAudio(const AudioData* audio) {
+}
+
+// 音声データをPCMファイルとして保存する関数
+void saveAsPcm(const char* outputFilename, const AudioData* audio) {
+}
